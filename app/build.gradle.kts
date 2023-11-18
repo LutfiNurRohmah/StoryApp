65db17d53bddd194775da55079d6f0cf92f8e86a
LutfiNurRohmah <lutfi0036ft.2021@student.uny.ac.id>
2023-11-18 15:30:27 +0700

    test: add testing when paging data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,4 +65,9 @@ dependencies {
 
     implementation("androidx.paging:paging-runtime-ktx:3.1.0")
 
+    testImplementation("androidx.arch.core:core-testing:2.1.0") // InstantTaskExecutorRule
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.1") //TestDispatcher
+    testImplementation("org.mockito:mockito-core:3.12.4")
+    testImplementation("org.mockito:mockito-inline:3.12.4")
+
 }
